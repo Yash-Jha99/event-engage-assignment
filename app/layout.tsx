b9c05ba622f862fa6533f6768c70cfb99ffe0c49
Yash Jha <jhayash99@gmail.com>
2023-05-06 15:11:53 +0530

    project setup with nextjs, typescript, scss and ant design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.scss'
+import 'antd/dist/reset.css'
+// import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
